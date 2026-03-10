3489a53f470dbb8156c009560a1489746f5c41ac
dhimashdr <dhimash55@gmail.com>
2026-03-10 14:55:33 +0700

    Add effect to the navigations

--- app/ui/components/navlinks.tsx
+++ app/ui/components/navlinks.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation"
 import Link from "next/link"
 import { HomeIcon as HomeOutline, BookOpenIcon as LibraryOutline, Square3Stack3DIcon as StuffOutline, NewspaperIcon as BlogOutline, UserIcon as AboutOutline } from "@heroicons/react/24/outline"
 import { HomeIcon as HomeSolid, BookOpenIcon as LibrarySolid, Square3Stack3DIcon as StuffSolid, NewspaperIcon as BlogSolid, UserIcon as AboutSolid } from "@heroicons/react/24/solid"
+import { motion as m } from "framer-motion"
 
 const navs = [
     {
@@ -38,6 +39,8 @@ const navs = [
     }
 ]
 
+const MLink = m(Link)
+
 export default function NavLinks(){
     const pathname = usePathname()
 
@@ -46,9 +49,9 @@ export default function NavLinks(){
             const NavOutline = nav.outline
             const NavSolid = nav.solid
 
-            return <Link href={nav.url} key={nav.title} className={`${pathname.includes(nav.url) && (nav.url !== "/" || pathname === "/") ? 'bg-blue-500/30' : 'bg-none'} px-2 transition-all duration-200 hover:bg-blue-500/30 ease-in rounded-sm my-auto mx-auto`}>
+            return <MLink initial={{scale: 1}} whileTap={{scale: 0.9}} transition={{ease: 'easeOut', duration: 0.1}} href={nav.url} key={nav.title} className={`${pathname.includes(nav.url) && (nav.url !== "/" || pathname === "/") ? 'bg-blue-500/30' : 'bg-none'} px-2 transition-all duration-200 hover:bg-blue-500/30 ease-in rounded-sm my-auto mx-auto`}>
                     {pathname.includes(nav.url) && (nav.url !== "/" || pathname === "/")? <NavSolid className="size-6"/>  : <NavOutline className="size-6"/>}
-            </Link>
+            </MLink>
         })}
     </div>
 }
